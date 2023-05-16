1c9aa7251f9868fa3954db08bc1f49886a0890fb
Rafael Solorio <rafavsgc@gmail.com>
2023-05-15 18:48:10 -0700

    Working on authentication

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const routes = require('express').Router()
 const users = require('./users');
 const recipes = require('./recipes');
 const passport = require('passport');
+const authController = require('../controllers/authenticate');
 
 
 routes.use('/users', users)
@@ -13,6 +14,12 @@ routes.get('/logout', function(req, res, next){
         if (err) { return next(err); }
         res.redirect('/')
     });
+});
+
+// Ruta para manejar el callback de la autenticación de GitHub
+routes.get('/github/callback', authController.handleCallback);
+routes.get('/', (req, res) => {
+    res.send(req.session.user != undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")
 });
 
 module.exports = routes
--- server.js
+++ server.js
@@ -30,34 +30,34 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
     })
     .use('/', require('./routes'))
 
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL
-},
-function(accessToken, refreshToken, profile, done){
-    return done(null, profile);
-}
-))
-
-passport.serializeUser((user, done) => {
-    done(null, user);
-})
-passport.deserializeUser((user, done) => {
-    done(null, user);
-});
-
-app.get('/', (req, res) => {
-    res.send(req.session.user != undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")
-});
-app.get('/github/callback', passport.authenticate('github', {
-    failureRedirect: 'api-docs', session: false}),
-    (rer, res) => {
-        req.session.user = req.user;
-        res.redirect('/');
-    }
-)
-    
+//passport.use(new GitHubStrategy({
+//    clientID: process.env.GITHUB_CLIENT_ID,
+//    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+//    callbackURL: process.env.CALLBACK_URL
+//},
+//function(accessToken, refreshToken, profile, done){
+//    return done(null, profile);
+//}
+//))
+//
+//passport.serializeUser((user, done) => {
+//    done(null, user);
+//})
+//passport.deserializeUser((user, done) => {
+//    done(null, user);
+//});
+//
+//app.get('/', (req, res) => {
+//    res.send(req.session.user != undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")
+//});
+//app.get('/github/callback', passport.authenticate('github', {
+//    failureRedirect: 'api-docs', session: false}),
+//    (rer, res) => {
+//        req.session.user = req.user;
+//        res.redirect('/');
+//    }
+//)
+  
 app.get('/health', (req, res) => {
     res.json({ status: 'ok' });
   });
